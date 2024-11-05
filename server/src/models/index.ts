e36ff646e22baeffe227f6f3db2c457c79b46fab
Avery Jacobson <averyajacobson@gmail.com>
2024-11-04 21:57:39 -0600

    updated config

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,6 +1,14 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+
+console.log("DB_URL:", process.env.DB_URL);
+console.log("DB_HOST:", process.env.DB_HOST);
+console.log("DB_PORT:", process.env.DB_PORT);
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_USER:", process.env.DB_USER);
+
+
 import { Sequelize } from 'sequelize';
 import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
@@ -25,6 +33,11 @@ const sequelize = process.env.DB_URL
         },
       }
     );
+
+    sequelize.authenticate()
+  .then(() => console.log('Database connection established successfully.'))
+  .catch((err) => console.error('Unable to connect to the database:', err));
+
 
 const User = UserFactory(sequelize);
 const Ticket = TicketFactory(sequelize);
