f52952acb08627cbe8185589930e1aae5384d918
Avery Jacobson <averyajacobson@gmail.com>
2024-11-05 09:48:12 -0600

    removed env and updated my database path

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -6,19 +6,15 @@ import { Sequelize } from 'sequelize';
 import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME || '',
-  process.env.DB_USER || '',
-  process.env.DB_PASSWORD || '',
-  {
-    host: process.env.DB_HOST || 'localhost',
-    port: Number(process.env.DB_PORT) || 5432,
-    dialect: 'postgres',
-    dialectOptions: {
-      decimalNumbers: true,
-    },
-  }
-);
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
+      host: 'localhost',
+      dialect: 'postgres',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
 
 
 const User = UserFactory(sequelize);
