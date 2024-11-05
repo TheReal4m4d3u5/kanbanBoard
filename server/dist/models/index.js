4bd8f78112cb601ba8fcf56bed4a9df67a5d660f
Avery Jacobson <averyajacobson@gmail.com>
2024-11-04 21:45:13 -0600

    updated index.js with new connection to db

--- server/dist/models/index.js
+++ server/dist/models/index.js
@@ -4,9 +4,15 @@ import { Sequelize } from 'sequelize';
 import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 const sequelize = process.env.DB_URL
-    ? new Sequelize(process.env.DB_URL)
-    : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
-        host: 'localhost',
+    ? new Sequelize(process.env.DB_URL, {
+        dialect: 'postgres',
+        dialectOptions: {
+            decimalNumbers: true,
+        },
+    })
+    : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD || '', {
+        host: process.env.DB_HOST || 'localhost',
+        port: Number(process.env.DB_PORT) || 5432,
         dialect: 'postgres',
         dialectOptions: {
             decimalNumbers: true,
--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -6,14 +6,25 @@ import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 
 const sequelize = process.env.DB_URL
-  ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD, {
-      host: 'localhost',
+  ? new Sequelize(process.env.DB_URL, {
       dialect: 'postgres',
       dialectOptions: {
         decimalNumbers: true,
       },
-    });
+    })
+  : new Sequelize(
+      process.env.DB_NAME || '',
+      process.env.DB_USER || '',
+      process.env.DB_PASSWORD || '',
+      {
+        host: process.env.DB_HOST || 'localhost',
+        port: Number(process.env.DB_PORT) || 5432,
+        dialect: 'postgres',
+        dialectOptions: {
+          decimalNumbers: true,
+        },
+      }
+    );
 
 const User = UserFactory(sequelize);
 const Ticket = TicketFactory(sequelize);
