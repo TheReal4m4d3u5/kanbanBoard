425fa899b6bc7fc0d6cc32aa2feddde73a3e7ab8
Avery Jacobson <averyajacobson@gmail.com>
2024-11-04 22:08:47 -0600

    updated path to db

--- server/dist/models/index.js
+++ server/dist/models/index.js
@@ -9,20 +9,25 @@ import { Sequelize } from 'sequelize';
 import { UserFactory } from './user.js';
 import { TicketFactory } from './ticket.js';
 const sequelize = process.env.DB_URL
-    ? new Sequelize(process.env.DB_URL, {
-        dialect: 'postgres',
-        dialectOptions: {
-            decimalNumbers: true,
-        },
+  ? new Sequelize(process.env.DB_URL, {
+      dialect: 'postgres',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
     })
-    : new Sequelize(process.env.DB_NAME || '', process.env.DB_USER || '', process.env.DB_PASSWORD || '', {
+  : new Sequelize(
+      process.env.DB_NAME || '',
+      process.env.DB_USER || '',
+      process.env.DB_PASSWORD || '',
+      {
         host: process.env.DB_HOST || 'localhost',
         port: Number(process.env.DB_PORT) || 5432,
         dialect: 'postgres',
         dialectOptions: {
-            decimalNumbers: true,
+          decimalNumbers: true,
         },
-    });
+      }
+    );
 sequelize.authenticate()
     .then(() => console.log('Database connection established successfully.'))
     .catch((err) => console.error('Unable to connect to the database:', err));
